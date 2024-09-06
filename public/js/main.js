3d0f73cddc2ffe3f39bc0e85b363701727deb220
dylanyng <dylanyoung@hey.com>
2024-09-06 19:11:07 -0400

    Readme update

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,12 @@
+// Time zone detection
 function detectTimezone() {
   const timezoneInput = document.getElementById('timezone');
+  // Check if timezone element is on page
   if (timezoneInput) {
     const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     timezoneInput.value = timezone;
   }
 }
-
 if (document.readyState === 'loading') {
   document.addEventListener('DOMContentLoaded', detectTimezone);
 } else {
