6a57c2b415904bb2e22a352a3d4763371889504e
dylanyng <dylanyoung@hey.com>
2024-09-06 23:06:26 -0400

    Begin tailwind styling on base layout and index

--- server.js
+++ server.js
@@ -57,5 +57,5 @@ app.use('/migraines', migraineRoutes)
 app.use(errorHandler)
  
 app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+    console.log(`Server running on port ${process.env.PORT}`)
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./views/**/*.ejs",
     "./public/**/*.js",
   ],
   theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.neutral,
+        indigo: colors.indigo,
+        red: colors.rose,
+        yellow: colors.amber,
+      }
+    },
     extend: {},
   },
   plugins: [],
 }
-
