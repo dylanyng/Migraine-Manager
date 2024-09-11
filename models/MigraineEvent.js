0f29ea2fe76eb297cb8dec7ecea2eab29ed1334b
dylanyng <dylanwadeyoung@me.com>
2024-09-11 18:14:50 -0400

    Change pain scale and improve pain level form input

--- models/MigraineEvent.js
+++ models/MigraineEvent.js
@@ -18,7 +18,7 @@ const MigraineEventSchema = new mongoose.Schema({
   },
   painLevel: {
     type: Number,
-    enum: [0, 1, 2, 3, 4],
+    enum: [0, 1, 2, 3, 4, 5],
     required: false
   },
   painLocation: {
