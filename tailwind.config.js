f8b25383a12e343a6b32c14ec3918e63c4a4272a
dylanyng <dylanyoung@hey.com>
2024-09-14 22:07:44 -0400

    Remove pain level from cards if null

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       colors: {
         midnight: 'rgb(17, 24, 39)',
+        deadnight: '#0C111C'
       },
       screens: {
         'xs': '375px',  // Target iPhone SE and other small devices
