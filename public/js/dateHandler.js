0dbeb721e205b1262fa60413f32a58f2bffff6ca
dylanyng <dylanwadeyoung@me.com>
2024-09-18 17:52:10 -0400

    Refactor dateHander.js to make functionality more modular

--- public/js/dateHandler.js
+++ public/js/dateHandler.js
@@ -1,16 +1,15 @@
-function dateHandler() {
-  // Date picker
-  const dateInput = document.getElementById('date');
+// Target the date picker on the page
+const dateInput = document.getElementById('date');
 
-  // Get today's date in YYYY-MM-DD format, based on user's time zone
-  const today = new Date().toLocaleString("sv").split(' ')[0];
+// Get today's date in YYYY-MM-DD format, based on user's time zone
+const today = new Date().toLocaleString("sv").split(' ')[0];
 
-  // Set the default value to today
+// Set date picker value to today
+function setDateAsToday() {
   dateInput.value = today;
-
-  // Restrict future dates from selection
-  dateInput.setAttribute('max', today);
 }
 
-// Call the function when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', dateHandler);
+// Restrict future dates from selection
+function restrictFutureDates() {
+  dateInput.setAttribute('max', today);
+}
