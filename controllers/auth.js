3a975f80059c1a22e205541da73e7f1126f31d36
dylanyng <dylanwadeyoung@me.com>
2026-01-16 12:28:26 -0500

    Remove unused flash error from auth.js and simplify email check

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,13 +16,16 @@ const User = require('../models/User')
   
   exports.postLogin = (req, res, next) => {
     const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
-  
+    
+    if (req.body.email && !validator.isEmail(req.body.email)) {
+      validationErrors.push({ msg: 'Please enter a valid email address.' })
+    }
+
     if (validationErrors.length) {
       req.flash('error', validationErrors)
       return res.redirect('/login')
     }
+
     req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
   
     passport.authenticate('local', (err, user, info) => {
